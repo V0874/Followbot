6a736fcf2e954329b41abc03d6543c8f22ffdb46
V0874 <visthemasteranon@gmail.com>
2025-12-16 21:41:34 -0600

    successfully implemented a basic differential drive system with proportional steering. need to work on the integral and derivative portion so we have a complete PID controller and abstract it into an interface. the tests went great and the robot follows the line excellent compared to previous test runs.

--- src/main.c
+++ src/main.c
@@ -17,22 +17,34 @@ int main() {
     // set up pwm signals
     enable_pwm(TIMER3, TIMER16BIT_FAST_PWM_8BIT, 
     TIMER16BIT_64PRESCALER, TIMER16BIT_CLEAR_OCN_MODE);
+
+    int base_speed = 50;
+    int set_point = 0;
+    int proportional_gain = 40;
+
     
     while(1) {
 
         int right_sensor = readPin(&SENSOR1_PIN_REG, SENSOR1_PIN);
         int left_sensor = readPin(&SENSOR2_PIN_REG, SENSOR2_PIN);
 
-            if (!right_sensor) {
-            drive_motor1(TIMER3, &MOTOR1_DIRECTION_PORT, MOTOR1_DIRECTION_PIN, 35);
-            } else {
-                slow_motor3(TIMER3, 0);
-            }
+        int process_variable = right_sensor - left_sensor;
+        int error = set_point - process_variable;
+        
+        int proportional_speed = error * proportional_gain;
 
-            if (!left_sensor) {
-            drive_motor2(TIMER3, &MOTOR2_DIRECTION_PORT, MOTOR2_DIRECTION_PIN, 35);
-            } else {
-                slow_motor1(TIMER3, 0);
-            }
+        int left_motor_speed = base_speed + proportional_speed;
+        int right_motor_speed = base_speed - proportional_speed;
+
+        if(error < 0){
+            drive_motor1(TIMER3, &MOTOR1_DIRECTION_PORT, MOTOR1_DIRECTION_PIN, left_motor_speed);
+            drive_motor2(TIMER3, &MOTOR2_DIRECTION_PORT, MOTOR2_DIRECTION_PIN, right_motor_speed);
+        } else if (error > 0) {
+            drive_motor1(TIMER3, &MOTOR1_DIRECTION_PORT, MOTOR1_DIRECTION_PIN, left_motor_speed);
+            drive_motor2(TIMER3, &MOTOR2_DIRECTION_PORT, MOTOR2_DIRECTION_PIN, right_motor_speed);
+        } else {
+            forward(TIMER3, &MOTOR1_DIRECTION_PORT, &MOTOR2_DIRECTION_PORT,
+            MOTOR1_DIRECTION_PIN, MOTOR2_DIRECTION_PIN, base_speed);    
+        }
     }
 }
